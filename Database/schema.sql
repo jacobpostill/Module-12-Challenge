2ed1c634f624f9a3a9bc38b2d937764645c073e0
Jacob Postill <postilljacob@gmail.com>
2023-02-20 15:11:25 -0500

    added databases

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,18 +1,32 @@
-DROP DATABASE IF EXISTS movie_db;
-CREATE DATABASE movie_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-USE movie_db;
+USE employee_db;
 
-CREATE TABLE movies(
-  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  movie_name VARCHAR(100) NOT NULL
+CREATE TABLE department(
+  department_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE reviews(
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    movie_id INT,
-    review TEXT,
-    FOREIGN KEY (movie_id)
-    REFERENCES movies(id)
-    ON DELETE SET NULL
+CREATE TABLE roles(
+  role_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id)
+  REFERENCES department(department_id)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employees(
+  employees_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT NULL,
+  FOREIGN KEY (manager_id)
+  REFERENCES employees(employees_id)
+  FOREIGN KEY (role_id)
+  REFERENCES roles(role_id)
+  ON DELETE SET NULL
 );
--- Database/seeds.sql
+++ Database/seeds.sql
@@ -1,26 +1,31 @@
-INSERT INTO movies (movie_name)
-VALUES  ("Harry Potter 1"),
-        ("Harry Potter 2"),
-        ("Harry Potter 3"),
-        ("Harry Potter 4"),
-        ("Harry Potter 5"),
-        ("Harry Potter 6"),
-        ("Harry Potter 7"),
-        ("Harry Potter 8");
 
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ('Chase', 'Stokes', 1, NULL),
+       ('Rudy', 'Pankow', 2, 1),
+       ('Sarah', 'Cameron', 3, 2),
+       ('Darth', 'Vader', 4, NULL),
+       ('Luke', 'Skywalker', 5, 3),
+       ('Harry', 'Potter', 6, 4),
+       ('Ron', 'Weasle', 4, NULL),
+       ('Froddo', 'Baggins', 2, NULL),
+       ('Jon', 'Snow', 1, NULL),
+       ('Ned', 'Stark', 6, 5),
+       ('Arya', 'Stark', 5, NULL),
 
+INSERT INTO department (name)
+VALUES ('Engineering'),
+       ('Legal');
+       ('Sales'),
+       ('HR'),
+       
 
+INSERT INTO roles (title, salary, department_id)
+VALUES ('Head Engineer', 200000, 1),
+       ('Chemical Engineer', 100000, 1),
+       ('Head Lawyer', 150000, 2),
+       ('Lawyer', 100000, 2);
+       ('Sales Lead', 100000, 3),
+       ('Sales Person', 80000, 3),
+       ('HR Manager', 90000, 4),
+       ('HR Associate', 75000, 4),
 
-
-INSERT INTO reviews (movie_id, review)
-VALUES  (1, "Great movie"),
-        (2, "Better then the 1st"),
-        (3, "Better then the 2nd"),
-        (4, "Better then the 3rd"),
-        (5, "Better then the 4th"),
-        (6, "Better then the 5th"),
-        (7, "Better then the 6th"),
-        (8, "Better then the 7th"),
-        (5, "Solid"),
-        (2, "cool"),
-        (8, "noice");
